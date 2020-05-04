e5c492c7ba90df06b2780063672ac2d2c410f140
Benjamin Alloway <balloway@meetveracity.com>
2020-05-04 14:10:46 -0400

    cleaned up state | added ability to create new todo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React from "react"
 import Header from "./components/Header"
 import TodoItem from "./components/TodoItem"
 
+function CreateTodo(props) {
+    const style = {
+        padding: "10px",
+        display: "flex",
+        justifyContent: "center"
+    }
+    return (
+        <form style={style} className="todo-item" onSubmit={(e) => props.handleFormSubmit(e)}>
+            <input placeholder="Create new todo" type="text" name="text" onChange={(e) => props.handleFormChange(e)} value={props.text} />
+            <input type="submit" value="Create" />
+        </form>
+    )
+}
+
 class App extends React.Component {
     // props are immutable, cannot change prop inside a component
     // state is different, in that a component can store and maintain it's own data
@@ -9,10 +23,31 @@ class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            todos: JSON.parse(window.localStorage.getItem('todoList')) || []
+            todos:  [],
+            todoForm: {
+                text: ""
+            }
         }
-        this.seedTodo = this.seedTodo.bind(this)
+        this.seedTodo = this.loadTodos.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.createTodo = this.createTodo.bind(this)
+        this.handleFormChange = this.handleFormChange.bind(this)
+        this.handleFormSubmit = this.handleFormSubmit.bind(this)
+    }
+
+    handleFormChange(event){
+        const value = event.target.value
+        this.setState({todoForm: {text: value}})
+    }
+
+    handleFormSubmit(event){
+        event.preventDefault();
+        this.createTodo();
+        this.setState({
+            todoForm: {
+                text: ""
+            }
+        })
     }
     
     handleChange(id) {
@@ -35,7 +70,30 @@ class App extends React.Component {
         })
     }
 
-    seedTodo() {
+    createTodo() {
+        if (this.state.todoForm.text !== "") {
+            let todos = this.state.todos
+            let id = 0;
+            todos.forEach(todo => {
+            if (todo.id >= id) {
+                id = todo.id + 1;
+            }
+            });
+            
+            const newTodo = {
+                id: id,
+                text: this.state.todoForm.text,
+                completed: false
+            }
+            
+            todos = [...todos, newTodo]
+            
+            window.localStorage.setItem('todoList', JSON.stringify(todos))
+            this.loadTodos()
+        }
+    }
+
+    loadTodos() {
         const todos = [
                 {
                     id: 1,
@@ -65,12 +123,17 @@ class App extends React.Component {
             ]
         if(window.localStorage.getItem('todoList') === null) {
             window.localStorage.setItem('todoList', JSON.stringify(todos))
-            this.setState({todos})
         }
+        this.setState({
+            todos: JSON.parse(window.localStorage.getItem('todoList'))
+        })
     }
 
     componentDidMount(){
-        this.seedTodo()
+        this.loadTodos()
+        // fetch("https://damp-feather-df5f.countries.workers.dev/?code=US")
+        //     .then(response => response.json())
+        //     .then(data => console.log(data))
     }
 
     render() {
@@ -81,6 +144,9 @@ class App extends React.Component {
                 <Header />
                 <div className="todo-list">
                     {todoComponents}
+                </div>
+                <div className="todo-list">
+                    <CreateTodo text={this.state.todoForm.text} handleFormSubmit={this.handleFormSubmit} handleFormChange={this.handleFormChange} />
                 </div>
             </div> 
         )
